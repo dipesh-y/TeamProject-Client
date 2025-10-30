11c925593389bdb543699dfca7f295e97248ce0a
vibhaj20 <vibhagowdaj20@gmail.com>
2025-10-30 23:44:54 +0530

    Vibha added sidemenue and both slider in the main page

--- src/components/Header/Navigation/CategoryPanel.jsx
+++ src/components/Header/Navigation/CategoryPanel.jsx
@@ -6,6 +6,9 @@ import { IoCloseSharp } from "react-icons/io5";
 import { FaRegPlusSquare } from "react-icons/fa";
 import { FiMinusSquare } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import logo from "../../../assets/logo.jpg";
+
+
 
 const CategoryPanel = (props) => {
   const [submenuIndex, setSubmenuIndex] = useState(null);
@@ -17,6 +20,7 @@ const CategoryPanel = (props) => {
 
   const openSubmenu = (index) => {
     setSubmenuIndex(submenuIndex === index ? null : index);
+    setInnerSubmenuIndex(null);
   };
 
   const openInnerSubmenu = (index) => {
@@ -25,24 +29,29 @@ const CategoryPanel = (props) => {
 
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" className="CategoryPanel">
-      <h3 className="p-3 text-[16px] font-[500] flex items-center justify-between">
+      <div className="p-3">
+         <Link to="/" className="flex justify-center">
+            <img src="https://serviceapi.spicezgold.com/download/1750047766437_logo.jpg" alt="Logo"
+                  className="w-[170px]"/>
+         </Link>
+
+                </div>
+      <h3 className="p-3 text-[18px] font-[500] flex items-center justify-between px-3">
         Shop By Categories
         <IoCloseSharp
           onClick={() => props.openCategoryPanel(false)}
           className="cursor-pointer text-[20px]"
         />
       </h3>
-
       <div className="scroll">
         <ul className="w-full">
-          {/* Category 1 */}
-          <li className="list-none flex items-center relative flex-col">
+          {/* Fashion */}
+          <li className="list-none flex items-center relative flex-col level-1 pl-0">
             <Link to="/" className="w-full">
-              <Button className="w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+              <Button className="w-full !text-left justify-start !pb-4  !px-3 !text-[rgba(0,0,0,0.8)]">
                 Fashion
               </Button>
             </Link>
-
             {submenuIndex === 0 ? (
               <FiMinusSquare
                 className="absolute top-[10px] right-[15px] cursor-pointer"
@@ -54,17 +63,15 @@ const CategoryPanel = (props) => {
                 onClick={() => openSubmenu(0)}
               />
             )}
-            {
-            submenuIndex === 0 && (
-              <ul className="submenu w-full pl-3">
-                <li className="list-none relative">
+            {submenuIndex === 0 && (
+              <ul className="submenu w-full">
+                {/* Women */}
+                <li className="list-none relative level-2 pl-3">
                   <Link to="/" className="w-full">
-                    <Button className="w-full !text-left justify-start 
-                    !px-3 !text-[rgba(0,0,0,0.8)]">
-                      Apparel
+                    <Button className="w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                      Women
                     </Button>
                   </Link>
-
                   {innerSubmenuIndex === 0 ? (
                     <FiMinusSquare
                       className="absolute top-[10px] right-[15px] cursor-pointer"
@@ -76,60 +83,65 @@ const CategoryPanel = (props) => {
                       onClick={() => openInnerSubmenu(0)}
                     />
                   )}
-
                   {innerSubmenuIndex === 0 && (
-                    <ul className="inner_submenu w-full pl-3">
-                      <li className="list-none relative mb-1">
-                        <Link
-                          to="/"
-                          className="link w-full !text-left justify-start !px-3 transition text-[14px]"
-                        >
-                          Smart Tablet
-                        </Link>
-                      </li>
-                      <li className="list-none relative mb-1">
-                        <Link
-                          to="/"
-                          className="link w-full !text-left justify-start !px-3 transition text-[14px]"
-                        >
-                          Crepe T-Shirt
-                        </Link>
-                      </li>
-                      <li className="list-none relative mb-1">
-                        <Link
-                          to="/"
-                          className="link w-full !text-left justify-start !px-3 transition text-[14px]"
-                        >
-                          Leather Watch
-                        </Link>
-                      </li>
-                      <li className="list-none relative mb-1">
-                        <Link
-                          to="/"
-                          className="link w-full !text-left justify-start !px-3 transition text-[14px]"
-                        >
-                          Rolling Diamond
+                    <ul className="inner_submenu w-full">
+                      <li className="list-none relative mb-1 level-3 pl-6">
+                        <Link to="/" className="link w-full !text-left justify-start !px-3 transition text-[14px]">
+                          Sarees
                         </Link>
                       </li>
                     </ul>
                   )}
-
-                  
+                </li>
+                {/* Girls */}
+                <li className="list-none relative level-2 pl-3">
+                  <Link to="/" className="w-full">
+                    <Button className="w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                      Girls
+                    </Button>
+                  </Link>
+                  {innerSubmenuIndex === 1 ? (
+                    <FiMinusSquare
+                      className="absolute top-[10px] right-[15px] cursor-pointer"
+                      onClick={() => openInnerSubmenu(1)}
+                    />
+                  ) : (
+                    <FaRegPlusSquare
+                      className="absolute top-[10px] right-[15px] cursor-pointer"
+                      onClick={() => openInnerSubmenu(1)}
+                    />
+                  )}
+                  {innerSubmenuIndex === 1 && (
+                    <ul className="inner_submenu w-full">
+                      <li className="list-none relative mb-1 level-3 pl-6">
+                        <Link to="/" className="link w-full !text-left justify-start !px-3 transition text-[14px]">
+                          Kurtas & Suits
+                        </Link>
+                      </li>
+                      <li className="list-none relative mb-1 level-3 pl-6">
+                        <Link to="/" className="link w-full !text-left justify-start !px-3 transition text-[14px]">
+                          Tops
+                        </Link>
+                      </li>
+                    </ul>
+                  )}
+                </li>
+                {/* Men */}
+                <li className="list-none relative mb-1 level-2 pl-3">
+                  <Link to="/" className="w-full">
+                    <Button className="w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                      Men
+                    </Button>
+                  </Link>
                 </li>
               </ul>
             )}
           </li>
-          <li className="list-none flex items-center relative flex-col">
+          {/* Electronics */}
+          <li className="list-none flex items-center relative flex-col level-1 pl-0">
             <Link to="/" className="w-full">
-              <Button className="w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+              <Button className="!pb-4 w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
                 Electronics
-              </Button>
-            </Link></li>
-
-            <li className="list-none flex items-center relative flex-col">
-            <Link to="/" className="w-full">
-              <Button className="w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
-                Bags
               </Button>
             </Link>
             {submenuIndex === 1 ? (
@@ -143,45 +155,139 @@ const CategoryPanel = (props) => {
                 onClick={() => openSubmenu(1)}
               />
             )}
-
-            
-
-            
-            
-            
-            </li>
-
-            
-
-            <li className="list-none flex items-center relative flex-col">
+            {submenuIndex === 1 && (
+              <ul className="submenu w-full">
+                {/* Mobile */}
+                <li className="list-none relative level-2 pl-3">
+                  <Link to="/" className="w-full">
+                    <Button className="w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                      Mobile
+                    </Button>
+                  </Link>
+                  {innerSubmenuIndex === 0 ? (
+                    <FiMinusSquare
+                      className="absolute top-[10px] right-[15px] cursor-pointer"
+                      onClick={() => openInnerSubmenu(0)}
+                    />
+                  ) : (
+                    <FaRegPlusSquare
+                      className="absolute top-[10px] right-[15px] cursor-pointer"
+                      onClick={() => openInnerSubmenu(0)}
+                    />
+                  )}
+                  {innerSubmenuIndex === 0 && (
+                    <ul className="inner_submenu w-full">
+                      <li className="list-none relative mb-1 level-3 pl-6">
+                        <Link to="/" className="link w-full !text-left justify-start !px-3 transition text-[14px]">
+                          OnePlus
+                        </Link>
+                      </li>
+                      <li className="list-none relative mb-1 level-3 pl-6">
+                        <Link to="/" className="link w-full !text-left justify-start !px-3 transition text-[14px]">
+                          OPPO
+                        </Link>
+                      </li>
+                      <li className="list-none relative mb-1 level-3 pl-6">
+                        <Link to="/" className="link w-full !text-left justify-start !px-3 transition text-[14px]">
+                          Vivo
+                        </Link>
+                      </li>
+                    </ul>
+                  )}
+                </li>
+                {/* Smartwatch */}
+                <li className="list-none relative mb-1 level-2 pl-3">
+                  <Link to="/" className="w-full">
+                    <Button className="w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                      Smartwatch
+                    </Button>
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </li>
+          {/* Bags */}
+          <li className="list-none flex items-center relative flex-col level-1 pl-0">
+            <Link to="/" className="w-full">
+              <Button className=" !pb-4 w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                Bags
+              </Button>
+            </Link>
+          </li>
+          {/* Footwear */}
+          <li className="list-none flex items-center relative flex-col level-1 pl-0">
             <Link to="/" className="w-full">
               <Button className="w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
                 Footwear
               </Button>
-            </Link></li>
-
-            <li className="list-none flex items-center relative flex-col">
+            </Link>
+            {submenuIndex === 2 ? (
+              <FiMinusSquare
+                className="absolute top-[10px] right-[15px] cursor-pointer"
+                onClick={() => openSubmenu(2)}
+              />
+            ) : (
+              <FaRegPlusSquare
+                className="absolute top-[10px] right-[15px] cursor-pointer"
+                onClick={() => openSubmenu(2)}
+              />
+            )}
+            {submenuIndex === 2 && (
+              <ul className="submenu w-full">
+                <li className="list-none relative mb-1 level-2 pl-3">
+                  <Link to="/" className="w-full">
+                    <Button className="w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                      Men Footwear
+                    </Button>
+                  </Link>
+                </li>
+                <li className="list-none relative mb-1 level-2 pl-3">
+                  <Link to="/" className="w-full">
+                    <Button className="w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                      Women Footwear
+                    </Button>
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </li>
+          {/* Groceries */}
+          <li className="list-none flex items-center relative flex-col level-1 pl-0">
+            <Link to="/" className="w-full">
+              <Button className=" !pb-4 w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                Groceries
+              </Button>
+            </Link>
+          </li>
+          {/* Beauty */}
+          <li className="list-none flex items-center relative flex-col level-1 pl-0">
+            <Link to="/" className="w-full">
+              <Button className=" !pb-4 w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                Beauty
+              </Button>
+            </Link>
+          </li>
+          {/* Wellness */}
+          <li className="list-none flex items-center relative flex-col level-1 pl-0">
+            <Link to="/" className="w-full">
+              <Button className=" !pb-4 w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
+                Wellness
+              </Button>
+            </Link>
+          </li>
+          {/* Jewellery */}
+          <li className="list-none flex items-center relative flex-col level-1 pl-0">
             <Link to="/" className="w-full">
               <Button className="w-full !text-left justify-start !px-3 !text-[rgba(0,0,0,0.8)]">
                 Jewellery
               </Button>
-            </Link></li>
-
-            
-
-
+            </Link>
+          </li>
         </ul>
-
- 
-        
       </div>
     </Box>
   );
 
-
-
-  
-  
   return (
     <Drawer open={props.isOpenCatPanel} onClose={toggleDrawer(false)}>
       {DrawerList}
@@ -189,9 +295,4 @@ const CategoryPanel = (props) => {
   );
 };
 
-
 export default CategoryPanel;
-
-
-
-
---
+++ src/components/HomeCatSlider/index.jsx
@@ -0,0 +1,168 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Link } from "react-router-dom";
+import { Navigation } from 'swiper/modules';
+
+const HomeCatSlider = () => {
+  return (
+    <div className=" py-3 homeCatSlider">
+      <div className="container">
+        <Swiper
+            slidesPerView={8}
+            spaceBetween={30}
+            navigation={true}
+            modules={[Navigation]}
+            className="mySwiper"
+        >
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525204708_fash.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Fashion</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525218436_ele.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Electronics</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525231018_bag.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Bags</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525239704_foot.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Footware</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525248057_gro.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Groceries</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525255799_beauty.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Beauty</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525275367_well.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Wellness</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525286186_jw.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Jewellery</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525204708_fash.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Fashion</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525218436_ele.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Electronics</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525231018_bag.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Bags</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525239704_foot.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Footware</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525248057_gro.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Groceries</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525255799_beauty.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Beauty</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525275367_well.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Wellness</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <Link to='/'>
+                    <div className="item py-7 px-3 bg-white rounded-sm text-center flex items-center justify-center flex-col">
+                        <img src="https://api.spicezgold.com/download/file_1734525286186_jw.png" className=" w-[60px] transition-all" />
+                        <h3 className="text-[15px] font-[500] mt-3">Jewellery</h3>
+                    </div>
+                </Link>
+            </SwiperSlide>
+                    
+        </Swiper>
+      </div>
+    </div>
+  );
+}
+
+export default HomeCatSlider;
